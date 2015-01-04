5d097a09788222321889ba4f6b4f45dea1f8f0a0
Marco Loiodice <mloiodice13@gmail.com>
2015-01-04 19:14:29 +0100

    Ajout des champs demandés dans la base de données avec les getters et setters pour les Users.

--- Copro-PierreLouve/src/AppBundle/Entity/User.php
+++ Copro-PierreLouve/src/AppBundle/Entity/User.php
@@ -27,6 +27,14 @@ class User extends BaseUser
      * @ORM\OneToMany(targetEntity="AppBundle\Entity\Actu", mappedBy="auteur")
      **/
     private $actus;
+    private $nom;
+    private $prenom;
+    private $adresse;
+    private $mail1;
+    private $mail2;
+    private $telephone1;
+    private $telephone2;
+    private $millieme;
 
     public function __construct()
     {
@@ -78,5 +86,93 @@ class User extends BaseUser
     public function getActus()
     {
         return $this->actus;
+    }
+    
+    /**
+    * nom
+    */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+    
+    /**
+    * prenom
+    */
+    public function getPrenom()
+    {
+        return $this->prenom;
+    }
+    
+    /**
+    * adresse
+    */
+    public function getAdresse()
+    {
+        return $this->adresse;
+    }
+    
+    /**
+    * millieme
+    */
+    public function getMillieme()
+    {
+        return $this->millieme;
+    }
+    
+    /**
+    * mail-1
+    */
+    public function getMail1()
+    {
+        return $this->mail1;
+    }
+    
+    public function setMail1()
+    {
+    	$this->mail1 = $mail1;
+    	return $this;
+    }
+    
+    /**
+    * mail-2
+    */
+    public function getMail2()
+    {
+        return $this->mail2;
+    }
+    
+    public function setMail2()
+    {
+    	$this->mail2 = $mail2;
+    	return $this;
+    }
+    
+    /**
+    * telephone-1
+    */
+    public function getTelephone1()
+    {
+        return $this->telephone1;
+    }
+    
+    public function settelephone1()
+    {
+    	$this->telephone1 = $telephone1;
+    	return $this;
+    }
+    
+    /**
+    * telephone-2
+    */
+    public function getTelephone2()
+    {
+        return $this->telephone2;
+    }
+    
+    public function settelephone2()
+    {
+    	$this->telephone2 = $telephone2;
+    	return $this;
     }
 }
