5d1a4592b9289fb303ed5cf5822b23d60e597adc
Carine Pic <pic.carine@gmail.com>
2015-01-12 19:25:48 +0100

    Améliorations, mise en page... 1

--- Copro-PierreLouve/web/js/call.jCarousel.js
+++ Copro-PierreLouve/web/js/call.jCarousel.js
@@ -1,3 +1,28 @@
-$(document).ready(function(){
-	$('#pagetop').jCarousel();
+$(window).load(function() {
+	$(document).ready(function() {
+		$('.diaporama')
+			.jcarousel(
+				{
+					wrap: 'circular'
+				})
+			.jcarouselAutoscroll(
+				{
+			       target: '+=1',
+			       interval: 3000
+				}
+			);
+
+		$('.diaporama-pagination')
+			.on('jcarouselpagination:active', 'a', function() {
+				$(this).addClass('active');
+			})
+			.on('jcarouselpagination:inactive', 'a', function() {
+				$(this).removeClass('active');
+			})
+			.jcarouselPagination({
+				'item': function(page) {
+					return '<a class="page" href="#' + page + '">' + page + '</a>';
+				}
+			});
+	});
 });
